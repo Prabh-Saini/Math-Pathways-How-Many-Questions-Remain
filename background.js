269a10db814a55056aad243bad3dde1cb28fb3b8
Prabh <64233660+Prabh-Saini@users.noreply.github.com>
2022-11-13 22:38:27 +1100

    added total questions and code is much nicer

--- background.js
+++ background.js
@@ -1,15 +1,19 @@
 function execute() {
 	// check
-	if (!window.location.href.includes("/work")) return alert("This extension is meant to be used in Math Pathways. If this is a error, please contact Syntax Error#3955 on Discord."); 
+	if (!window.location.href.includes("/work")) {
+		return alert("This extension is meant to be used in Math Pathways. If this is a error, please submit an issue on Github (github.com/Prabh-Saini/howmanyquestionsremain)."); 
+	}
 
-	// get variables
-	let remaining = document.getElementsByClassName("progress-bar-item pb-grey-bg").length;
-	let total = document.getElementsByClassName("progress-bar-item pb-blue-bg").length + remaining;
+	// variables
+	let x = "progress-bar-item pb-", 
+   	    y = "-bg ng-star-inserted",
+   	    z = document.getElementsByClassName(`${x}grey${y}`).length;
 
 	// extension onClick
-	if (remaining) alert(`Remaining questions: ${remaining}\nTotal questions: ${total}`)
-	else {
-		throw new Error("Data can't be retrieved");
+	if (remaining) {
+		alert(`Remaining questions: ${z}\nTotal questions: ${document.getElementsByClassName(`${x}blue${y}`).length + z}`)
+	} else {
+		throw new Error("Error: Unable to retrieve data.");
 		return alert(`Error! Unable to retrieve data.`);
 	}
 }
