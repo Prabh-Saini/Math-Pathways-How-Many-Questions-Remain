e30661ed7f871827520a10ab6a6aec3a4e3be4a2
Prabh <64233660+Prabh-Saini@users.noreply.github.com>
2022-11-13 22:46:04 +1100

    code looks fire

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
 function execute() {
-	// check
-	if (!window.location.href.includes("/work")) {
+	// variables
+	let blue = document.getElementsByClassName("progress-bar-item pb-blue-bg ng-star-inserted").length,
+   	    grey = document.getElementsByClassName("progress-bar-item pb-grey-bg ng-star-inserted").length,
+	    total = blue + grey;
+	
+	// verify
+	if (!window.location.href.includes("mpclass.com/work")) {
 		return alert("This extension is meant to be used in Math Pathways. If this is a error, please submit an issue on Github (github.com/Prabh-Saini/howmanyquestionsremain)."); 
 	}
 
-	// variables
-	let x = "progress-bar-item pb-", 
-   	    y = "-bg ng-star-inserted",
-   	    z = document.getElementsByClassName(`${x}grey${y}`).length;
 
 	// extension onClick
 	if (remaining) {
-		alert(`Remaining questions: ${z}\nTotal questions: ${document.getElementsByClassName(`${x}blue${y}`).length + z}`)
+		alert(`Remaining questions: ${grey}\nTotal questions: ${total}`)
 	} else {
 		throw new Error("Error: Unable to retrieve data.");
 		return alert(`Error! Unable to retrieve data.`);
